07c78b6602225de9834ac3c86a35e626f3266f9a
robertvala <52976962+robertvala@users.noreply.github.com>
2021-04-15 08:03:12 -0500

    get valores del adapter

--- app/src/main/java/com/example/operacionesmteriasprimas/Adapters/AdapterRecyclerActividades.java
+++ app/src/main/java/com/example/operacionesmteriasprimas/Adapters/AdapterRecyclerActividades.java
@@ -1,51 +1,140 @@
 package com.example.operacionesmteriasprimas.Adapters;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.operacionesmteriasprimas.Modelos.Operador;
+import com.example.operacionesmteriasprimas.R;
 import com.example.operacionesmteriasprimas.ui.reporte.ListaActividadOperadores;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class AdapterRecyclerActividades extends RecyclerView.Adapter<AdapterRecyclerActividades.ViewHolder> {
+public class AdapterRecyclerActividades extends BaseAdapter {
     private List<String> mdata;
-    private LayoutInflater mInflater;
     private Context context;
-    HashMap<String, Operador> valores;
-    private ArrayList<Integer> mSectionPositions;
+    HashMap<Integer, Double> valores;
+    int posicion;
     private ListaActividadOperadores instance;
+    Double horas;
 
-    public AdapterRecyclerActividades(List<String> mdata, LayoutInflater mInflater, Context context, HashMap<String, Operador> valores, ArrayList<Integer> mSectionPositions, ListaActividadOperadores instance) {
+    public AdapterRecyclerActividades(List<String> mdata,  Context context, HashMap<Integer, Double> valores, int posicion, ListaActividadOperadores instance) {
         this.mdata = mdata;
-        this.mInflater = mInflater;
         this.context = context;
         this.valores = valores;
-        this.mSectionPositions = mSectionPositions;
+        this.posicion=posicion;
         this.instance = instance;
     }
 
-    @NonNull
+
     @Override
-    public AdapterRecyclerActividades.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public int getCount() {
+        return mdata.size();
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdapterRecyclerActividades.ViewHolder holder, int position) {
-
+    public Object getItem(int position) {
+        return mdata.get(position);
     }
 
     @Override
-    public int getItemCount() {
+    public long getItemId(int position) {
         return 0;
+    }
+
+    public HashMap<Integer,Double> getValores(){
+        return valores;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(context).inflate(R.layout.adaptador_reportes_diarios, null);
+        String item=(String)getItem(position);
+        TextView txtEnunciado=convertView.findViewById(R.id.txtNombreActividad);
+        TextInputEditText tietHoras=convertView.findViewById(R.id.editTextHoras);
+        Button btnSiguient=convertView.findViewById(R.id.btnSiguiente);
+        Button btnAnterior=convertView.findViewById(R.id.btnAnterio);
+
+        if(valores.containsKey(posicion)){
+            tietHoras.setText(String.valueOf(valores.get(posicion)));
+        }
+
+        else{
+            tietHoras.setText("");
+        }
+
+        txtEnunciado.setText(item);
+        tietHoras.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()>0){
+                    horas=Double.parseDouble(s.toString());
+                }
+                else{
+                    horas=0.0;
+
+                }
+                if(horas>0.0){
+                    valores.put(posicion,horas);
+                    instance.marcarChips(posicion);
+                }
+                else{
+                   valores.remove(posicion);
+                }
+            }
+        });
+
+        btnSiguient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean siguiente=instance.siguiente(item);
+                if(siguiente){
+                    btnSiguient.setEnabled(true);
+                    btnSiguient.setTextColor(context.getColor(R.color.purple_200));
+                }
+                else{
+                    btnSiguient.setEnabled(false);
+                    btnSiguient.setTextColor(context.getColor(R.color.tab));
+                }
+            }
+        });
+
+        btnAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean siguiente=instance.anterior(item);
+                if(siguiente){
+                    btnAnterior.setEnabled(true);
+                    btnAnterior.setTextColor(context.getColor(R.color.purple_200));
+                }
+                else{
+                    btnAnterior.setEnabled(false);
+                    btnAnterior.setTextColor(context.getColor(R.color.tab));
+                }
+            }
+        });
+        return convertView;
     }
 }
--- app/src/main/java/com/example/operacionesmteriasprimas/ui/reporte/ListaActividadOperadores.java
+++ app/src/main/java/com/example/operacionesmteriasprimas/ui/reporte/ListaActividadOperadores.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.operacionesmteriasprimas.Adapters.AdapterRecyclerActividades;
 import com.example.operacionesmteriasprimas.Modelos.Operador;
 import com.example.operacionesmteriasprimas.Modelos.Reporte;
 import com.example.operacionesmteriasprimas.R;
@@ -15,7 +16,9 @@ import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListaActividadOperadores extends AppCompatActivity {
     TextView txtNombreOperador,txtFecha,txtActividadesPendientes,txtModificarParametros;
@@ -25,7 +28,10 @@ public class ListaActividadOperadores extends AppCompatActivity {
     Operador operador=null;
     List<String> actividades;
     List<String> actividad;
+    int cont;
+    HashMap<Integer,Double> valores;
     Context context=this;
+    AdapterRecyclerActividades adapter;
 
 
     @Override
@@ -48,8 +54,8 @@ public class ListaActividadOperadores extends AppCompatActivity {
     }
 
     private void generarChips() {
-        actividad=new ArrayList<>();
-        int cont=1;
+
+        cont=1;
         for(String s:actividades){
             Chip chip= new Chip(context);
             chip.setTextSize(18);
@@ -58,7 +64,12 @@ public class ListaActividadOperadores extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     actividad.clear();
-                    actividad.add(chip.getText().toString());
+                    String nombreChip=chip.getText().toString();
+                    String[] sep=nombreChip.split(" ");
+                    int posicion=Integer.parseInt(sep[1])-1;
+                    actividad.add(actividades.get(posicion));
+                    valores=adapter.getValores();
+                    cargarLista();
                 }
             });
             chipGroup.addView(chip);
@@ -67,10 +78,60 @@ public class ListaActividadOperadores extends AppCompatActivity {
 
     }
 
+    public void marcarChips(Integer posicion){
+        valores=adapter.getValores();
+        Chip chip=(Chip) chipGroup.getChildAt(posicion);
+        chip.setChipBackgroundColorResource(R.color.purple_200);
+
+
+
+    }
+
+    public int getPosicionChip(Chip chip){
+        String nombreChip=chip.getText().toString();
+        String[] sep=nombreChip.split(" ");
+        int posicion=Integer.parseInt(sep[1])-1;
+        return posicion;
+    }
+
     private void cargarDatos() {
+        actividad=new ArrayList<>();
         txtFecha.setText(reporte.getFecha());
         txtNombreOperador.setText(operador.getNombre());
         txtActividadesPendientes.setText("Actividades por ingresar: "+operador.getActividadesPendientes());
+        actividad.add(actividades.get(0));
+        valores=operador.getActividades();
+        cargarLista();
 
+    }
+
+    void cargarLista(){
+        adapter= new AdapterRecyclerActividades(actividad,context,valores,actividades.indexOf(actividad.get(0)),this);
+        listView.setAdapter(adapter);
+    }
+
+    public boolean anterior(String item){
+        int index=actividades.indexOf(item)-1;
+        if(index>=0){
+            actividad.clear();
+            actividad.add(actividades.get(index));
+            cargarLista();
+            return true;
+        }
+        else{return false; }
+    }
+
+    public boolean siguiente(String item){
+        int maxIndex=actividades.size()-1;
+        int index=actividades.indexOf(item)+1;
+        if(index<=maxIndex){
+            actividad.clear();
+            actividad.add(actividades.get(index));
+            cargarLista();
+            return  true;
+        }
+        else{
+            return false;
+        }
     }
 }
