100b9b9be5b952f84743febd1d06cec8e61a1312
ppereira123 <52977467+ppereira123@users.noreply.github.com>
2021-04-23 12:10:10 -0500

    sigue mal

--- app/src/main/java/com/example/operacionesmteriasprimas/InformeVista.java
+++ app/src/main/java/com/example/operacionesmteriasprimas/InformeVista.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.operacionesmteriasprimas.Adapters.adaptadorVistaHoras;
 import com.example.operacionesmteriasprimas.Modelos.Operador;
@@ -18,7 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.Repo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -49,9 +49,12 @@ public class InformeVista extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot ds: snapshot.getChildren()){
+                    Log.d("myTag", "entra a la base de datos");
                     GenericTypeIndicator<Reporte> t = new GenericTypeIndicator<Reporte>() {};
                     Reporte m = ds.getValue(t);
+                    Toast.makeText(context, m.toString(), Toast.LENGTH_SHORT).show();
                     reportes.add(m);
+
 
 
                 }
@@ -63,34 +66,76 @@ public class InformeVista extends AppCompatActivity {
             }
 
         });
+        Log.d("myTag", reportes.toString());
 
         listareportes=reportes;
 
-
-
     }
     private List<sumas> GetData() {
-        HashMap<String, Double> actividadeshoras = new HashMap<String, Double>();
+        //HashMap<String, Double> actividadeshoras = new HashMap<String, Double>();
+        List<sumas> lista=new ArrayList<sumas>();
 
         cargardatos();
         for (Reporte reporte:listareportes){
             for(Operador operador:listReporteToHash(reporte.getOperadores())){
                 int i=0;
                 for (String actividad:operador.getNombreActividades()){
+                    if(existeactividad(lista,actividad)==-1){
+                        sumas suma=new sumas(actividad,operador.getActividades().get(i));
+                        lista.add(suma);
+
+                    }
+                    else {
+
+                        Double horas= (Double) (operador.getActividades().get(i)+lista.get(existeactividad(lista,actividad)).getHoras());
+                        sumas suma=new sumas(actividad,horas);
+                        lista.add(suma);
+
+                    }
+                    i=i++;
+
+
+                }
+
+
+
+                /*
+                int i=0;
+                for (String actividad:operador.getNombreActividades()){
                     if (actividadeshoras.containsKey(actividad)){
                         double horas=actividadeshoras.get(actividad)+operador.getActividades().get(i);
                         actividadeshoras.put(actividad,horas);
-                        Log.d("myTag", "si");
+
                     }else {
-                        actividadeshoras.put(actividad,actividadeshoras.get(actividad));
-                        Log.d("myTag", "no");
+
+                        actividadeshoras.put(actividad, operador.getActividades().get(i));
+
                     }
-                    i=0+i;
+                    i=1+i;
                 }
+
+                 */
             }
 
+
         }
-        return listsumaToHash(actividadeshoras);
+
+
+
+        return lista;
+    }
+    private int existeactividad(List<sumas> list, String actividad){
+        int posicion=-1;
+        int i=0;
+        for(sumas x:list){
+            if(x.getActividad().equals(actividad)){
+                posicion=i;
+            }
+            i=i++;
+        }
+        return posicion;
+
+
     }
     private List<Operador> listReporteToHash(HashMap<String, Operador> hashoperador) {
         List<Operador> lista= new ArrayList<>();
